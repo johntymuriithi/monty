174689f850ff23dceb2df9c8d2558ac2dc0aeaaa
johntymuriithi <muriithijohn634@gmail.com>
2023-09-24 10:19:09 -0700

    file

--- another.c
+++ another.c
@@ -10,7 +10,6 @@ void op_nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
-
 /**
  * op_add - Add the top two elements of the stack.
  * @stack: Double pointer to the top of the stack.
@@ -18,34 +17,17 @@ void op_nop(stack_t **stack, unsigned int line_number)
  */
 void op_add(stack_t **stack, unsigned int line_number)
 {
-	int num1, num2, sum;
-	stack_t *new_node;
+    int num1, num2, result;
+    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	if (stack == NULL || *stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't add empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	num1 = (*stack)->n;
-	num2 = (*stack)->next->n;
-	sum = num1 + num2;
+    num2 = (*stack)->n;
+    num1 = (*stack)->next->n;
+    result = num1 + num2;
 
-	op_pop(stack, line_number);
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = sum;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-
-	*stack = new_node;
+    op_pop(stack, line_number); 
+    (*stack)->n = result;
 }
-

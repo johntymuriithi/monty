d0502d869dd4b7b311f4b1c5f5a9084de4cb9701
johntymuriithi <muriithijohn634@gmail.com>
2023-09-24 08:31:16 -0700

    file

--- helper2.c
+++ helper2.c
@@ -10,14 +10,20 @@
  */
 void op_swap(stack_t **stack, unsigned int line_number)
 {
-	int swa;
+	int swa, num;
+	stack_t *current = *stack;
 
-	if (stack == NULL || *stack == NULL)
+	while (current != NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		num++;
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+	if (num < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
 	swa = (*stack)->n;
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = swa;
